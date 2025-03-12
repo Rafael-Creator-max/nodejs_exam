0f780714edff6dd39b7a88fb1716c3c6b7e6e811
Rafael-Creator-max <rafael.pereztarira@student.kuleuven.be>
2025-03-12 10:31:55 +0100

    2 . Added a moongose model for snippets

---
+++ nodejs-snippets-api/src/models/Snippet.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+// interface
+export interface ISnippet extends Document {
+  title: string;
+  code: string;
+  language: string;
+  tags: string[];
+  expiresIn?: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+//  Mongoose schema
+const SnippetSchema: Schema = new Schema(
+  {
+    title: { type: String, required: true },
+    code: { type: String, required: true },
+    language: { type: String, required: true },
+    tags: { type: [String], default: [] },
+    expiresIn: { type: Number, default: null },
+  },
+  { timestamps: true } // createdAt & updatedAt
+);
+
+export default mongoose.model<ISnippet>("Snippet", SnippetSchema);
